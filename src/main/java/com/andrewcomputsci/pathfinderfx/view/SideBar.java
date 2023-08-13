28ffb88c8402c9560d9f6ff9c054c18642ec410b
Andrew Pegg <andrewcomputsci2019@gmail.com>
2023-08-12 18:34:30 -0700

    finished with tileType ui box area, now starting the algorithm section

--- src/main/java/com/andrewcomputsci/pathfinderfx/view/SideBar.java
+++ src/main/java/com/andrewcomputsci/pathfinderfx/view/SideBar.java
@@ -1,6 +1,7 @@
 package com.andrewcomputsci.pathfinderfx.view;
 
 import com.andrewcomputsci.pathfinderfx.Model.CellType;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
@@ -13,43 +14,76 @@ public class SideBar {
     private ScrollPane scrollPane;
     private Button changeGridSizeButton;
     private TextField gridSizeTextField;
-    private VBox gridSizeWrapper;
+    private HBox gridSizeLabelWrapper;
+    private VBox gridSizeGroupWrapper;
+    private HBox tileTypeWrapper = new HBox();
     private ComboBox<CellType> tileTypeComboBox;
 
     public SideBar() {
         rootContainer = new VBox();
         rootContainer.setAlignment(Pos.TOP_CENTER);
         rootContainer.setStyle("-fx-border-width: 1px; -fx-border-color: red;");
-        rootContainer.setSpacing(25.0);
+        rootContainer.setSpacing(12.5);
         scrollPane = new ScrollPane();
         scrollPane.setFitToHeight(true);
         scrollPane.setFitToWidth(true);
         scrollPane.setContent(rootContainer);
         scrollPane.setStyle("-fx-border-width: 1px; -fx-border-color: red;");
         initGridSizeUi();
+        initTilePicker();
     }
 
     private void initGridSizeUi() {
-        gridSizeWrapper = new VBox();
-        gridSizeWrapper.setAlignment(Pos.CENTER);
-        gridSizeWrapper.setPrefHeight(65.0);
-        gridSizeWrapper.setStyle("-fx-border-width: 2px; -fx-border-radius: 5px; -fx-border-color: gray;");
-        HBox box = new HBox();
-        box.setAlignment(Pos.CENTER);
-        VBox.setMargin(gridSizeWrapper,new Insets(2.5));
-        box.setSpacing(5.0);
-        Label label =  new Label();
-        label.setFont(new Font(13));
-        label.setText("Grid Size: ");
+        gridSizeGroupWrapper = new VBox();
+        gridSizeGroupWrapper.setAlignment(Pos.CENTER);
+        gridSizeGroupWrapper.setPrefHeight(65.0);
+        gridSizeGroupWrapper.setStyle("-fx-border-width: 2px; -fx-border-radius: 5px; -fx-border-color: gray;");
+        gridSizeLabelWrapper = new HBox();
+        gridSizeLabelWrapper.setAlignment(Pos.CENTER);
+        VBox.setMargin(gridSizeGroupWrapper,new Insets(2.5));
+        gridSizeLabelWrapper.setSpacing(2.5);
+        Label gridSizeLabel =  new Label();
+        gridSizeLabel.setFont(new Font(13));
+        gridSizeLabel.setText("Grid Size: ");
         gridSizeTextField = new TextField();
         gridSizeTextField.setPromptText("25,25");
-        box.getChildren().addAll(label,gridSizeTextField);
-        changeGridSizeButton = new Button("Change Grid Size");
-        gridSizeWrapper.getChildren().addAll(box, changeGridSizeButton);
-        gridSizeWrapper.setSpacing(5.0);
-        rootContainer.getChildren().add(gridSizeWrapper);
+        gridSizeLabelWrapper.getChildren().addAll(gridSizeLabel,gridSizeTextField);
+        changeGridSizeButton = new Button("Change Current Grid");
+        changeGridSizeButton.setFont(new Font(13));
+        gridSizeGroupWrapper.getChildren().addAll(gridSizeLabelWrapper, changeGridSizeButton);
+        gridSizeGroupWrapper.setSpacing(4.0);
+        rootContainer.getChildren().add(gridSizeGroupWrapper);
     }
-    public void initTilePicker(){
+    private void initTilePicker(){
+        tileTypeWrapper = new HBox();
+        tileTypeWrapper.setPrefHeight(65.0);
+        tileTypeWrapper.setSpacing(5.0);
+        tileTypeWrapper.setStyle("-fx-border-width: 2px; -fx-border-radius: 5px; -fx-border-color: gray;");
+        tileTypeWrapper.setAlignment(Pos.CENTER_LEFT);
+        VBox.setMargin(tileTypeWrapper,new Insets(2.5));
+        Label tileTypeLabel = new Label("TileType: ");
+        tileTypeLabel.setFont(new Font(13));
+        tileTypeComboBox = new ComboBox<>();
+        tileTypeComboBox.setCellFactory(param -> new ListCell<>(){
+
+            @Override
+            protected void updateItem(CellType item, boolean empty) {
+                super.updateItem(item, empty);
+                if(empty){
+                    setText(null);
+
+                }else {
+                    setText(item.name());
+                }
+            }
+        });
+        tileTypeComboBox.setPrefWidth(140);
+        tileTypeComboBox.setItems(FXCollections.observableArrayList(CellType.Source,CellType.Target,CellType.Wall,CellType.Traversable));
+        tileTypeComboBox.getSelectionModel().selectFirst();
+        tileTypeWrapper.getChildren().addAll(tileTypeLabel,tileTypeComboBox);
+        rootContainer.getChildren().add(tileTypeWrapper);
+    }
+    private void initAlgorithmPicker(){
 
     }
 
