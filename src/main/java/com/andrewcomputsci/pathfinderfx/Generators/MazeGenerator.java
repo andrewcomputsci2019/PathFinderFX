4cde5054de729e05c81c07d905c1d039afda8894
Andrew Pegg <andrewcomputsci2019@gmail.com>
2023-08-17 12:20:36 -0700

    added maze generation interface

---
+++ src/main/java/com/andrewcomputsci/pathfinderfx/Generators/MazeGenerator.java
@@ -0,0 +1,7 @@
+package com.andrewcomputsci.pathfinderfx.Generators;
+
+import com.andrewcomputsci.pathfinderfx.view.CellRectangle;
+
+public interface MazeGenerator {
+    public void generateMaze(CellRectangle[] grid);
+}
