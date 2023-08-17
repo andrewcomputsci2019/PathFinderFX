12dcc3010281970708cd9d94c23722a01ffe8596
Andrew Pegg <andrewcomputsci2019@gmail.com>
2023-08-17 12:20:18 -0700

    added File for bfs greedy

---
+++ src/main/java/com/andrewcomputsci/pathfinderfx/Solver/BFSGreedy.java
@@ -0,0 +1,12 @@
+package com.andrewcomputsci.pathfinderfx.Solver;
+
+import com.andrewcomputsci.pathfinderfx.view.CellRectangle;
+
+import java.util.List;
+
+public class BFSGreedy implements PathFinderSolver{
+    @Override
+    public List<CellRectangle> solve(CellRectangle[] grid, int width, int height) {
+        return null;
+    }
+}
