a00861e0026cbd20b2d06762aec28d2362134801
Andrew Pegg <andrewcomputsci2019@gmail.com>
2023-08-20 10:22:21 -0700

    added record for Statistics section

---
+++ src/main/java/com/andrewcomputsci/pathfinderfx/Model/Statistics.java
@@ -0,0 +1,9 @@
+package com.andrewcomputsci.pathfinderfx.Model;
+
+import com.andrewcomputsci.pathfinderfx.view.CellRectangle;
+
+import java.util.List;
+
+public record Statistics(List<CellRectangle> path, int passes, long deltaTime, int pathCost) {
+
+}
