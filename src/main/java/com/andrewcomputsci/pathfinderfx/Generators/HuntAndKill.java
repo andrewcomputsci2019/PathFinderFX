409416f87c11e1a3e05a4114ec6d61c6b054da23
Andrew Pegg <andrewcomputsci2019@gmail.com>
2023-09-15 12:09:25 -0700

    fixed small bug and improved formatting

--- src/main/java/com/andrewcomputsci/pathfinderfx/Generators/HuntAndKill.java
+++ src/main/java/com/andrewcomputsci/pathfinderfx/Generators/HuntAndKill.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class HuntAndKill implements MazeGenerator{
+public class HuntAndKill implements MazeGenerator {
     private boolean[] visited;
     private int width;
     private int height;
@@ -26,100 +26,102 @@ public class HuntAndKill implements MazeGenerator{
         this.grid = grid;
         messages = messageQueue;
         visited = new boolean[grid.length];
-        Arrays.fill(visited,false);
-        int startX = random.nextInt(0,width);
-        int startY = random.nextInt(0,height);
-        startX = startX%2==0?startX:startX-1;
-        startY = startY%2==0?startY:startY-1;
+        Arrays.fill(visited, false);
+        int startX = random.nextInt(0, width);
+        int startY = random.nextInt(0, height);
+        startX = startX % 2 == 0 ? startX : startX - 1;
+        startY = startY % 2 == 0 ? startY : startY - 1;
         walk(startX, startY);
-        while (true){
+        while (true) {
             boolean found = false;
-            for(int y = 0; y < height; y++){
-                if(y%2!=0)continue;
-                if(found)break;
-                for(int x = 0; x < width; x++){
-                    if(x%2!=0)continue;
-                    if(!visited[y*width+x] && hasVisitedAdjacentCell(x,y)){
+            for (int y = 0; y < height; y++) {
+                if (y % 2 != 0) continue;
+                if (found) break;
+                for (int x = 0; x < width; x++) {
+                    if (x % 2 != 0) continue;
+                    if (!visited[y * width + x] && hasVisitedAdjacentCell(x, y)) {
                         found = true;
-                        List<int[]> visitedCells = visitedCells(x,y);
+                        List<int[]> visitedCells = visitedCells(x, y);
                         int[] loc = visitedCells.get(random.nextInt(0, visitedCells.size()));
-                        joinCells(x,y,loc[0],loc[1]);
-                        walk(x,y);
+                        joinCells(x, y, loc[0], loc[1]);
+                        walk(x, y);
                         break;
                     }
                 }
             }
-            if(!found){
+            if (!found) {
                 break;
             }
         }
     }
 
-    private void walk(int x, int y){
+    private void walk(int x, int y) {
         List<int[]> cells;
-        visited[y*width+x] = true;
-        while (!(cells = unvisitedAdjacentCells(x,y)).isEmpty()){
-            visited[y*width+x] = true;
-            messages.add(new Message(grid[y*width+x], CellState.Current));
+        visited[y * width + x] = true;
+        while (!(cells = unvisitedAdjacentCells(x, y)).isEmpty()) {
+            visited[y * width + x] = true;
+            messages.add(new Message(grid[y * width + x], CellState.Current));
             int[] next = cells.get(random.nextInt(0, cells.size()));
-            joinCells(x,y,next[0],next[1]);
-            messages.add(new Message(grid[y*width+x],CellType.Traversable));
+            joinCells(x, y, next[0], next[1]);
+            messages.add(new Message(grid[y * width + x], CellType.Traversable));
             x = next[0];
             y = next[1];
         }
     }
 
-    private boolean hasVisitedAdjacentCell(int x, int y){
+    private boolean hasVisitedAdjacentCell(int x, int y) {
 
-       return (((x-2)>-1)&&visited[y*width+(x-2)]) || ((x+2 < width) && visited[y*width+x+2]) || ((y-2 > -1) && visited[(y-2)*width+x]) || ((y+2 < height) && visited[(y+2)*width+x]);
+        return (((x - 2) > -1) && visited[y * width + (x - 2)]) || ((x + 2 < width) && visited[y * width + x + 2]) || ((y - 2 > -1) && visited[(y - 2) * width + x]) || ((y + 2 < height) && visited[(y + 2) * width + x]);
     }
 
-    private List<int[]> unvisitedAdjacentCells(int x, int y){
+    private List<int[]> unvisitedAdjacentCells(int x, int y) {
         ArrayList<int[]> list = new ArrayList<>();
-        if(x-2 > -1 && ! visited[y*width+x-2]){
-            list.add(new int[]{x-2,y});
+        if (x - 2 > -1 && !visited[y * width + x - 2]) {
+            list.add(new int[]{x - 2, y});
         }
-        if(x+2 < width && ! visited[y*width+x+2]){
-            list.add(new int[]{x+2,y});
+        if (x + 2 < width && !visited[y * width + x + 2]) {
+            list.add(new int[]{x + 2, y});
         }
-        if(y-2 > -1 && ! visited[(y-2)*width+x]){
-            list.add(new int[]{x,y-2});
+        if (y - 2 > -1 && !visited[(y - 2) * width + x]) {
+            list.add(new int[]{x, y - 2});
         }
-        if(y+2 < width && ! visited[(y+2)*width+x]){
-            list.add(new int[]{x,y+2});
+        if (y + 2 < height && !visited[(y + 2) * width + x]) {
+            list.add(new int[]{x, y + 2});
         }
         return list;
     }
-    private List<int[]> visitedCells(int x, int y){
+
+    private List<int[]> visitedCells(int x, int y) {
         ArrayList<int[]> list = new ArrayList<>();
-        if(x-2 > -1 &&  visited[y*width+x-2]){
-            list.add(new int[]{x-2,y});
+        if (x - 2 > -1 && visited[y * width + x - 2]) {
+            list.add(new int[]{x - 2, y});
         }
-        if(x+2 < width && visited[y*width+x+2]){
-            list.add(new int[]{x+2,y});
+        if (x + 2 < width && visited[y * width + x + 2]) {
+            list.add(new int[]{x + 2, y});
         }
-        if(y-2 > -1 &&  visited[(y-2)*width+x]){
-            list.add(new int[]{x,y-2});
+        if (y - 2 > -1 && visited[(y - 2) * width + x]) {
+            list.add(new int[]{x, y - 2});
         }
-        if(y+2 < width &&  visited[(y+2)*width+x]){
-            list.add(new int[]{x,y+2});
+        if (y + 2 < height && visited[(y + 2) * width + x]) {
+            list.add(new int[]{x, y + 2});
         }
         return list;
     }
-    private void joinCells(int x1, int y1, int x2, int y2){
+
+    private void joinCells(int x1, int y1, int x2, int y2) {
         int x = x2;
         int y = y2;
-        if(x2 > x1){
-            x-=1;
-        }else if(x1 > x2){
-            x+=1;
+        if (x2 > x1) {
+            x -= 1;
+        } else if (x1 > x2) {
+            x += 1;
         }
-        if(y2 > y1){
-            y-=1;
-        }else if(y1 > y2){
-            y+=1;
+        if (y2 > y1) {
+            y -= 1;
+        } else if (y1 > y2) {
+            y += 1;
         }
-        visited[y*width+x] = true;
-        messages.add(new Message(grid[y*width+x], CellType.Traversable));
+        visited[y * width + x] = true;
+        messages.add(new Message(grid[y * width + x], CellType.Traversable));
     }
 }
